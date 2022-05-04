131f2bc6b37fa1ecfa3f7e1ce683ecbe3fe7f709
Krystian Laubach <krystian.laubach@gmail..com>
2022-05-04 22:16:38 +0100

    Initial Commit

---
+++ site/web/app/themes/sage/app/filters.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Theme filters.
+ */
+
+namespace App;
+
+/**
+ * Add "… Continued" to the excerpt.
+ *
+ * @return string
+ */
+add_filter('excerpt_more', function () {
+    return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
+});
+
+/**
+ * Disable Gutenberg Editor.
+ */
+add_filter('use_block_editor_for_post', '__return_false', 10);
